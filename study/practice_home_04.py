65e46a24776136faae6db89e8722bf015db1e96a
hwanhee <baldozaa@gmail.com>
2023-03-05 17:28:16 +0900

    practice

---
+++ study/practice_home_04.py
@@ -0,0 +1,40 @@
+import numpy as np
+from tensorflow.keras.models import Sequential
+from tensorflow.keras.layers import Dense
+from sklearn.model_selection import train_test_split
+import matplotlib.pyplot as plt
+from sklearn.metrics import r2_score
+from sklearn.datasets import load_diabetes
+
+#1. 데이터
+
+datasets=load_diabetes()
+x=datasets.data
+y=datasets.target
+
+x_train, x_test, y_train, y_test = train_test_split(
+    x, y,
+    train_size=0.9,
+    random_state=1203
+)
+
+#2. 모델 구성
+model=Sequential()
+model.add(Dense(20,input_dim=10))
+model.add(Dense(30))
+model.add(Dense(20))
+model.add(Dense(10))
+model.add(Dense(1))
+
+#3. 컴파일, 훈련
+model.compile(loss='mse', optimizer='adam')
+model.fit(x_train,y_train, epochs=100, batch_size=1)
+
+#4. 평가, 예측
+loss= model.evaluate(x_test,y_test)
+print('loss :', loss)
+
+y_predict=model.predict(x_test)
+r2=r2_score(y_test,y_predict)
+print('r2 :', r2)
+
