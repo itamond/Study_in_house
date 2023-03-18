acf16a08da48db279cec7d2ebd27738e743b8a06
hwanhee <baldozaa@gmail.com>
2023-03-18 11:32:11 +0000

    깨작깨작

---
+++ practice/dd.py
@@ -0,0 +1,13 @@
+#cabinet = {3:"유재석", 100:"김태호"}
+#print(cabinet[3])
+#print(cabinet[100])
+
+#print(cabinet.get(3))
+#print(cabinet.get(5, "사용 가능"))
+#print("hi")
+
+#print(3 in cabinet) # True
+#print(5 in cabinet) # False
+
+cabinet = {"A-3":"유재석", "B-100":"김태호"}
+print(cabinet)
